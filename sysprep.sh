39e61369c39d84bbf883c40cf0d8e7f29050d126
Erno Rigo <erno@rigo.info>
2020-02-05 18:55:53 +0100

    initial

--- sysprep.sh
+++ sysprep.sh
@@ -1,14 +1,6 @@
-#/bin/bash
-#git config --global user.email "hallgato@itseclabs.local"
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo add-apt-repository universe
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
-sudo apt-get install -y  dotnet-sdk-3.0 python3-pip nodejs npm
-sudo pip3 install pipenv
-#in pipenv shell:
-dotnet tool install -g dotnet-try
-#see: https://devblogs.microsoft.com/dotnet/net-core-with-juypter-notebooks-is-here-preview-1/
-pipenv run jupyter contrib nbextension install --user
+#!/usr/bin/env bash
+set -e +x
+cd $(dirname $0)
 pipenv run jupyter nbextensions_configurator enable --user
+pipenv run jupyter labextension install @jupyter-widgets/jupyterlab-manager
+touch .flag-sysprep-done

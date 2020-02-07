0aebb872c84527e8574ccd65708dbd6239b9aa7a
Erno Rigo <erno@rigo.info>
2020-02-07 09:35:59 +0100

    initial

--- sysprep.sh
+++ sysprep.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e +x
 cd $(dirname $0)
+pipenv run jupyter serverextension enable --py jupyterlab_git
 pipenv run jupyter nbextensions_configurator enable --user
 pipenv run jupyter labextension install @jupyter-widgets/jupyterlab-manager
+pipenv run jupyter labextension install @jupyterlab/git
+pipenv run jupyter lab build
 touch .flag-sysprep-done
